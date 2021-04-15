d08edcadb66539afa116ff70cd9df33f60c44a3e
DeGalan <SDG@cca.edu>
2021-04-14 17:34:18 -0700

    Favicon + Dark Mode!

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 console.log('Dark Mode');
 
-$(document).ready(function(){
-    $('.exercises').click(function(){
-        $(this).toggleClass('alt');
-    });
+$(document).ready(function() {
+	$("#darkmode").click(function() {
+		$('body').toggleClass('darkmode');
+        $('.projects').toggleClass('darkmode');
+        $('.exercises').toggleClass('darkmode');
+        $('footer').toggleClass('darkmode');
+        $('h1').toggleClass('darkmode');
+        $('.a1-e.darkmode:hover').toggleClass('darkmode');
+	});
 });
